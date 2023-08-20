08f366f694881e78e778025a2e034aecd6aa9bd6
ManojNadar <manojviru03@gmail.com>
2023-08-20 17:18:34 +0530

    admin

--- Controllers/RatingComments.js
+++ Controllers/RatingComments.js
@@ -1,10 +1,18 @@
 import Product from "../Model/ProductModel.js";
+import User from "../Model/UserModel.js";
 
 // Ratings
 
 export const addRatings = async (req, res) => {
   try {
     const { productId, rating } = req.body;
+
+    if (rating > 5) {
+      return res.status(404).json({
+        success: false,
+        message: "Not a valid Rating",
+      });
+    }
 
     const product = await Product.findByIdAndUpdate(
       productId,
@@ -33,9 +41,11 @@ export const addComments = async (req, res) => {
   try {
     const { productId, comment, userId } = req.body;
 
+    const user = await User.findById(userId);
+
     const product = await Product.findByIdAndUpdate(
       productId,
-      { $push: { comments: { comment, userId: userId } } },
+      { $push: { comments: { comment, userId: userId, name: user.name } } },
       { new: true }
     );
 
